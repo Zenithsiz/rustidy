da14358a6ca69445e4719555df929ded1b9d853f
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2026-01-01 22:04:36 +0000

    Optimized `MatchArms`'s parsing.

--- rustidy/src/ast/expr/with_block/match_.rs
+++ rustidy/src/ast/expr/with_block/match_.rs
@@ -14,7 +14,9 @@ use {
 			token,
 			with_attrs::{WithInnerAttributes, WithOuterAttributes},
 		},
+		parser::{ParseError, Parser, ParserError},
 	},
+	std::fmt,
 };
 
 /// `MatchExpression`
@@ -36,12 +38,95 @@ pub struct MatchExpression {
 pub struct Scrutinee(#[parse(with_tag = "skip:StructExpression")] Expression);
 
 /// `MatchArms`
+// TODO: Simplify this to just `Vec<MatchArmWithExpr<Expression, Option<token::Comma>>` and
+//       be careful parsing and formatting it?
 #[derive(PartialEq, Eq, Clone, Debug)]
 #[derive(serde::Serialize, serde::Deserialize)]
-#[derive(Parse, Format, Print)]
+#[derive(Format, Print)]
 pub struct MatchArms {
 	pub arms: Vec<MatchArmWithExprNonLast>,
-	pub last: Option<MatchArmWithExpr<Expression, Option<token::Comma>>>,
+	pub last: Option<MatchArmWithExpr<Box<Expression>, Option<token::Comma>>>,
+}
+
+impl Parse for MatchArms {
+	type Error = MatchArmsError;
+
+	fn name() -> Option<impl fmt::Display> {
+		None::<!>
+	}
+
+	fn parse_from(parser: &mut Parser) -> Result<Self, Self::Error> {
+		let mut arms = vec![];
+		let mut last = loop {
+			let Ok(arm) = parser.try_parse::<MatchArm>()? else {
+				break None;
+			};
+			let arrow = parser.parse::<token::FatArrow>()?;
+			let expr = parser.parse::<Expression>()?;
+			match expr {
+				Expression::WithoutBlock(expr) => match parser.try_parse::<token::Comma>()? {
+					Ok(trailing_comma) => arms.push(MatchArmWithExprNonLast::WithoutBlock(MatchArmWithExpr {
+						arm,
+						arrow,
+						expr,
+						trailing_comma,
+					})),
+					Err(_) =>
+						break Some(MatchArmWithExpr {
+							arm,
+							arrow,
+							expr: Box::new(Expression::WithoutBlock(expr)),
+							trailing_comma: None,
+						}),
+				},
+				Expression::WithBlock(expr) => {
+					let trailing_comma = parser.try_parse::<token::Comma>()?.ok();
+					arms.push(MatchArmWithExprNonLast::WithBlock(MatchArmWithExpr {
+						arm,
+						arrow,
+						expr,
+						trailing_comma,
+					}));
+				},
+			}
+		};
+
+		// Move the last arm to `last` if it's empty and it fits there.
+		if last.is_none() &&
+			let Some(arm) = arms.pop()
+		{
+			match arm {
+				MatchArmWithExprNonLast::WithoutBlock(_) => arms.push(arm),
+				MatchArmWithExprNonLast::WithBlock(arm) =>
+					last = Some(MatchArmWithExpr {
+						arm:            arm.arm,
+						arrow:          arm.arrow,
+						expr:           Box::new(Expression::WithBlock(arm.expr)),
+						trailing_comma: arm.trailing_comma,
+					}),
+			}
+		}
+
+		Ok(Self { arms, last })
+	}
+}
+
+#[derive(derive_more::Debug, derive_more::From, ParseError)]
+pub enum MatchArmsError {
+	#[parse_error(transparent)]
+	MatchArm(ParserError<MatchArm>),
+
+	#[parse_error(transparent)]
+	#[parse_error(fatal)]
+	FatArrow(ParserError<token::FatArrow>),
+
+	#[parse_error(transparent)]
+	#[parse_error(fatal)]
+	Expression(ParserError<Expression>),
+
+	#[parse_error(transparent)]
+	#[parse_error(fatal)]
+	Comma(ParserError<token::Comma>),
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -58,7 +143,7 @@ pub enum MatchArmWithExprNonLast {
 pub struct MatchArmWithExpr<E, C> {
 	pub arm:            MatchArm,
 	pub arrow:          token::FatArrow,
-	pub expr:           Box<E>,
+	pub expr:           E,
 	pub trailing_comma: C,
 }
 
