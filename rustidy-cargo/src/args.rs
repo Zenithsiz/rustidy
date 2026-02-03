ab4f52d0996ae88b2b03fd5a608ac1508f2fcede
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2026-02-03 15:57:54 +0000

    Added `rustidy-cargo` for cargo integration.

---
+++ rustidy-cargo/src/args.rs
@@ -0,0 +1,43 @@
+//! Arguments
+
+// Imports
+use std::{ffi::OsString, path::PathBuf};
+
+/// Rust-tidy formatter
+#[derive(Debug)]
+#[derive(clap::Parser)]
+#[clap(author, version, about)]
+#[clap(bin_name = "cargo")]
+#[command(styles = clap_cargo::style::CLAP_STYLING)]
+pub struct Args {
+	#[clap(subcommand)]
+	pub command: Command,
+}
+
+#[derive(Debug)]
+#[derive(clap::Subcommand)]
+pub enum Command {
+	Rustidy(Rustidy),
+}
+
+#[derive(Debug)]
+#[derive(clap::Args)]
+#[clap(author, version, about)]
+pub struct Rustidy {
+	/// Logs output to a file.
+	///
+	/// You can use `RUST_FILE_LOG` to set filtering options
+	#[clap(long = "log-file")]
+	pub log_file: Option<PathBuf>,
+
+	/// Specify package to format
+	#[arg(short = 'p', long = "package")]
+	pub packages: Vec<String>,
+
+	/// Specify path to Cargo.toml
+	#[arg(long = "manifest-path")]
+	pub manifest_path: Option<PathBuf>,
+
+	/// Arguments to pass to `rustidy`
+	pub extra_args: Vec<OsString>,
+}
---
+++ rustidy-cargo/src/main.rs
@@ -0,0 +1,126 @@
+//! Cargo integration for `rustidy`
+
+// Features
+#![feature(yeet_expr, exit_status_error)]
+
+// Module
+mod args;
+
+// Imports
+use {
+	self::args::Args,
+	app_error::{AppError, Context, app_error, ensure},
+	clap::Parser,
+	std::{
+		collections::HashSet,
+		env,
+		ffi::{OsStr, OsString},
+		io,
+		path::{Path, PathBuf},
+		process::{Command, ExitCode},
+	},
+	zutil_logger::Logger,
+};
+
+fn main() -> ExitCode {
+	match self::run() {
+		Ok(()) => ExitCode::SUCCESS,
+		Err(err) => {
+			tracing::error!("{}", err.pretty());
+			ExitCode::FAILURE
+		},
+	}
+}
+
+fn run() -> Result<(), AppError> {
+	// Initialize logging
+	let logger = {
+		let default_filters = |default| [(None, default)];
+		Logger::new(std::io::stderr, (), default_filters("info"), default_filters("debug"))
+	};
+
+	// Parse arguments
+	let args = Args::parse();
+	let args::Command::Rustidy(args) = args.command;
+	tracing::debug!(?args, "Arguments");
+
+	// Set logger file from arguments
+	logger.set_file(args.log_file.as_deref());
+
+	let packages = args.packages.into_iter().collect::<HashSet<_>>();
+	let manifest_path = match args.manifest_path {
+		Some(manifest_path) => {
+			let manifest_path = manifest_path
+				.canonicalize()
+				.context("Unable to canonicalize manifest path")?;
+			ensure!(
+				manifest_path.ends_with("Cargo.toml"),
+				"The manifest-path must point to a `Cargo.toml` file"
+			);
+			Some(manifest_path)
+		},
+		None => None,
+	};
+
+	self::format(packages, manifest_path.as_deref(), &args.extra_args)
+}
+
+fn format(packages: HashSet<String>, manifest_path: Option<&Path>, extra_args: &[OsString]) -> Result<(), AppError> {
+	// If we got no targets, error out
+	let targets = self::get_targets(packages, manifest_path)?;
+	ensure!(!targets.is_empty(), "No targets found for formatting");
+
+	let rustidy = env::var_os("RUSTIDY");
+	let rustidy = match &rustidy {
+		Some(rustidy) => rustidy,
+		None => OsStr::new("rustidy"),
+	};
+
+	Command::new(rustidy)
+		.args(targets)
+		.args(extra_args)
+		.status()
+		.map_err(|e| match e.kind() {
+			io::ErrorKind::NotFound =>
+				app_error!("Unable to find `rustidy` binary {rustidy:?}, ensure it's in your `$PATH`"),
+			_ => AppError::new(&e).context("Unable to spawn rustidy"),
+		})?
+		.exit_ok()
+		.context("rustidy returned an error")?;
+
+	Ok(())
+}
+
+/// Based on the specified `CargoFmtStrategy`, returns a set of main source files.
+fn get_targets(mut packages: HashSet<String>, manifest_path: Option<&Path>) -> Result<Vec<PathBuf>, AppError> {
+	let mut targets = vec![];
+
+	let metadata = {
+		let mut cmd = cargo_metadata::MetadataCommand::new();
+		cmd.no_deps();
+		if let Some(manifest_path) = manifest_path {
+			cmd.manifest_path(manifest_path);
+		}
+		cmd.exec().context("Unable to get cargo metadata")
+	}?;
+
+	for package in metadata.packages {
+		if !packages.is_empty() && !packages.remove(package.name.as_str()) {
+			continue;
+		}
+
+		for target in package.targets {
+			let target = target
+				.src_path
+				.canonicalize()
+				.context("Unable to get target source path")?;
+			targets.push(target);
+		}
+	}
+
+	for package in packages {
+		tracing::warn!("Package {package:?} wasn't found");
+	}
+
+	Ok(targets)
+}
