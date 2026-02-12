56cdc4a68d2b1c2cb2e2159ed2fcc87255650611
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2026-02-12 01:35:11 +0000

    Moved implementation of `AstStr` methods relating to it's representation to `AstStrRepr`.

--- rustidy-util/src/ast_str.rs
+++ rustidy-util/src/ast_str.rs
@@ -33,99 +33,36 @@ impl AstStr {
 	/// Returns the length of this string
 	#[must_use]
 	pub fn len(&self) -> usize {
-		match *self.repr() {
-			AstStrRepr::AstRange(range) => range.len(),
-			AstStrRepr::String(s) => s.len(),
-			AstStrRepr::Char(ch) => ch.len_utf8(),
-			AstStrRepr::Spaces { len } => len,
-			AstStrRepr::Indentation {
-				ref indent,
-				newlines,
-				depth,
-			} => newlines + depth * indent.len(),
-			AstStrRepr::Join { ref lhs, ref rhs } => lhs.len() + rhs.len(),
-			AstStrRepr::Dynamic(ref s) => s.len(),
-		}
+		self.repr().len()
 	}
 
 	/// Returns if this string is empty
 	#[must_use]
 	pub fn is_empty(&self) -> bool {
-		self.len() == 0
+		self.repr().is_empty()
 	}
 
 	/// Returns if this string is blank
 	#[must_use]
 	pub fn is_blank(&self, input: &str) -> bool {
-		match *self.repr() {
-			AstStrRepr::AstRange(range) => crate::is_str_blank(range.str(input)),
-			AstStrRepr::String(s) => crate::is_str_blank(s),
-			AstStrRepr::Char(ch) => ch.is_ascii_whitespace(),
-			AstStrRepr::Spaces { .. } | AstStrRepr::Indentation { .. } => true,
-			AstStrRepr::Join { ref lhs, ref rhs } => lhs.is_blank(input) && rhs.is_blank(input),
-			AstStrRepr::Dynamic(ref s) => crate::is_str_blank(s),
-		}
+		self.repr().is_blank(input)
 	}
 
 	/// Returns if this string has newlines
 	#[must_use]
 	pub fn has_newlines(&self, input: &str) -> bool {
-		match *self.repr() {
-			AstStrRepr::AstRange(range) => range.str(input).contains('\n'),
-			AstStrRepr::String(s) => s.contains('\n'),
-			AstStrRepr::Char(ch) => ch == '\n',
-			AstStrRepr::Spaces { .. } => false,
-			AstStrRepr::Indentation { newlines, .. } => newlines != 0,
-			AstStrRepr::Join { ref lhs, ref rhs } => lhs.has_newlines(input) && rhs.has_newlines(input),
-			AstStrRepr::Dynamic(ref s) => s.contains('\n'),
-		}
+		self.repr().has_newlines(input)
 	}
 
 	/// Returns if this string is equal to `other`
 	#[must_use]
 	pub fn is_str(&self, input: &str, other: &str) -> bool {
-		let repr = self.repr();
-		match *repr {
-			AstStrRepr::AstRange(range) => range.str(input) == other,
-			AstStrRepr::String(s) => s == other,
-			AstStrRepr::Dynamic(ref s) => s == other,
-
-			// TODO: Properly implement these to avoid allocating a string
-			_ => {
-				drop(repr);
-				self.str(input) == other
-			},
-		}
+		self.repr().is_str(input, other)
 	}
 
 	/// Writes this string
 	pub fn write(&self, input: &str, output: &mut String) {
-		match *self.repr() {
-			AstStrRepr::AstRange(range) => output.push_str(range.str(input)),
-			AstStrRepr::String(s) => output.push_str(s),
-			AstStrRepr::Char(ch) => output.push(ch),
-			AstStrRepr::Spaces { len } =>
-				for _ in 0..len {
-					output.push(' ');
-				},
-			AstStrRepr::Indentation {
-				ref indent,
-				newlines,
-				depth,
-			} => {
-				for _ in 0..newlines {
-					output.push('\n');
-				}
-				for _ in 0..depth {
-					output.push_str(indent);
-				}
-			},
-			AstStrRepr::Join { ref lhs, ref rhs } => {
-				lhs.write(input, output);
-				rhs.write(input, output);
-			},
-			AstStrRepr::Dynamic(ref s) => output.push_str(s),
-		}
+		self.repr().write(input, output);
 	}
 
 	/// Returns the string of this string
@@ -133,24 +70,7 @@ impl AstStr {
 	//       functions performing whatever checks the callers need instead?
 	#[must_use]
 	pub fn str<'input>(&self, input: &'input str) -> Cow<'input, str> {
-		let repr = self.repr();
-		match *repr {
-			AstStrRepr::AstRange(range) => range.str(input).into(),
-			AstStrRepr::String(s) => s.into(),
-			AstStrRepr::Char(ch) => ch.to_string().into(),
-			AstStrRepr::Dynamic(ref s) => s.clone().into(),
-
-			// Special case these to avoid a `String` allocation
-			AstStrRepr::Spaces { len: 0 } => "".into(),
-			AstStrRepr::Spaces { len: 1 } => " ".into(),
-
-			AstStrRepr::Spaces { .. } | AstStrRepr::Indentation { .. } | AstStrRepr::Join { .. } => {
-				drop(repr);
-				let mut output = String::new();
-				self.write(input, &mut output);
-				output.into()
-			},
-		}
+		self.repr().str(input)
 	}
 }
 
@@ -201,6 +121,125 @@ pub enum AstStrRepr {
 	// Dynamic string
 	// Note: Not `#[from]` to make it clear this is expensive
 	Dynamic(String),
+}
+
+impl AstStrRepr {
+	/// Returns the length of this string
+	#[must_use]
+	pub fn len(&self) -> usize {
+		match *self {
+			Self::AstRange(range) => range.len(),
+			Self::String(s) => s.len(),
+			Self::Char(ch) => ch.len_utf8(),
+			Self::Spaces { len } => len,
+			Self::Indentation {
+				ref indent,
+				newlines,
+				depth,
+			} => newlines + depth * indent.len(),
+			Self::Join { ref lhs, ref rhs } => lhs.len() + rhs.len(),
+			Self::Dynamic(ref s) => s.len(),
+		}
+	}
+
+	/// Returns if this string is empty
+	#[must_use]
+	pub fn is_empty(&self) -> bool {
+		self.len() == 0
+	}
+
+	/// Returns if this string is blank
+	#[must_use]
+	pub fn is_blank(&self, input: &str) -> bool {
+		match *self {
+			Self::AstRange(range) => crate::is_str_blank(range.str(input)),
+			Self::String(s) => crate::is_str_blank(s),
+			Self::Char(ch) => ch.is_ascii_whitespace(),
+			Self::Spaces { .. } | Self::Indentation { .. } => true,
+			Self::Join { ref lhs, ref rhs } => lhs.is_blank(input) && rhs.is_blank(input),
+			Self::Dynamic(ref s) => crate::is_str_blank(s),
+		}
+	}
+
+	/// Returns if this string has newlines
+	#[must_use]
+	pub fn has_newlines(&self, input: &str) -> bool {
+		match *self {
+			Self::AstRange(range) => range.str(input).contains('\n'),
+			Self::String(s) => s.contains('\n'),
+			Self::Char(ch) => ch == '\n',
+			Self::Spaces { .. } => false,
+			Self::Indentation { newlines, .. } => newlines != 0,
+			Self::Join { ref lhs, ref rhs } => lhs.has_newlines(input) && rhs.has_newlines(input),
+			Self::Dynamic(ref s) => s.contains('\n'),
+		}
+	}
+
+	/// Returns if this string is equal to `other`
+	#[must_use]
+	pub fn is_str(&self, input: &str, other: &str) -> bool {
+		match *self {
+			Self::AstRange(range) => range.str(input) == other,
+			Self::String(s) => s == other,
+			Self::Dynamic(ref s) => s == other,
+
+			// TODO: Properly implement these to avoid allocating a string
+			_ => self.str(input) == other,
+		}
+	}
+
+	/// Writes this string
+	pub fn write(&self, input: &str, output: &mut String) {
+		match *self {
+			Self::AstRange(range) => output.push_str(range.str(input)),
+			Self::String(s) => output.push_str(s),
+			Self::Char(ch) => output.push(ch),
+			Self::Spaces { len } =>
+				for _ in 0..len {
+					output.push(' ');
+				},
+			Self::Indentation {
+				ref indent,
+				newlines,
+				depth,
+			} => {
+				for _ in 0..newlines {
+					output.push('\n');
+				}
+				for _ in 0..depth {
+					output.push_str(indent);
+				}
+			},
+			Self::Join { ref lhs, ref rhs } => {
+				lhs.write(input, output);
+				rhs.write(input, output);
+			},
+			Self::Dynamic(ref s) => output.push_str(s),
+		}
+	}
+
+	/// Returns the string of this string
+	// TODO: This can be somewhat expensive, should we replace it with
+	//       functions performing whatever checks the callers need instead?
+	#[must_use]
+	pub fn str<'input>(&self, input: &'input str) -> Cow<'input, str> {
+		match *self {
+			Self::AstRange(range) => range.str(input).into(),
+			Self::String(s) => s.into(),
+			Self::Char(ch) => ch.to_string().into(),
+			Self::Dynamic(ref s) => s.clone().into(),
+
+			// Special case these to avoid a `String` allocation
+			Self::Spaces { len: 0 } => "".into(),
+			Self::Spaces { len: 1 } => " ".into(),
+
+			Self::Spaces { .. } | Self::Indentation { .. } | Self::Join { .. } => {
+				let mut output = String::new();
+				self.write(input, &mut output);
+				output.into()
+			},
+		}
+	}
 }
 
 impl<'de> serde::Deserialize<'de> for AstStrRepr {
