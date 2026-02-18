cbe01e6b81fdae029671363e08e4aa1cc761726f
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2026-02-18 09:34:15 +0000

    Optimized parsing of `Punctuation`.

--- rustidy-ast/src/token.rs
+++ rustidy-ast/src/token.rs
@@ -17,7 +17,7 @@ use {
 	},
 	rustidy_ast_util::{IdentifierOrKeyword, RawIdentifier},
 	rustidy_format::{Format, Formattable},
-	rustidy_parse::Parse,
+	rustidy_parse::{Parse, ParseError},
 	rustidy_print::Print,
 };
 
@@ -50,7 +50,7 @@ pub enum Token {
 /// `Punctuation`
 #[derive(PartialEq, Eq, Debug)]
 #[derive(serde::Serialize, serde::Deserialize)]
-#[derive(Parse, Formattable, Format, Print)]
+#[derive(Formattable, Format, Print)]
 pub enum Punctuation {
 	Eq(Eq),
 	Lt(Lt),
@@ -106,3 +106,175 @@ pub enum Punctuation {
 	BracesOpen(BracesOpen),
 	BracesClose(BracesClose),
 }
+
+impl rustidy_parse::Parse for Punctuation {
+	type Error = PunctuationError;
+
+	fn parse_from(parser: &mut rustidy_parse::Parser) -> Result<Self, Self::Error> {
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Eq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Lt(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Le(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::EqEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::PunctEquals(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Ge(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Gt(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::AndAnd(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::OrOr(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Not(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Tilde(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Plus(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Minus(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Star(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Slash(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Percent(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Caret(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::And(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Or(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Shl(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Shr(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::PlusEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::MinusEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::StarEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::SlashEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::PercentEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::CaretEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::AndEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::OrEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::ShlEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::ShrEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::At(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Dot(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::DotDot(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::DotDotDot(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::DotDotEq(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Comma(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Semi(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Colon(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::PathSep(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::RArrow(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::LArrow(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::FatArrow(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Pound(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Dollar(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Question(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::Underscore(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::ParenOpen(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::ParenClose(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::BracketOpen(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::BracketClose(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::BracesOpen(value));
+		}
+		if let Ok(value) = parser.try_parse().map_err(|_| PunctuationError)? {
+			return Ok(Self::BracesClose(value));
+		}
+
+		Err(PunctuationError)
+	}
+}
+
+#[derive(derive_more::Debug, ParseError)]
+#[parse_error(fmt = "Expected punctuation")]
+pub struct PunctuationError;
